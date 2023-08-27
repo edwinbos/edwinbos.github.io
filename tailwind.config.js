2a8e6b0335fea5466ffc75b489a79f866361960a
Edwin Bos <edwinbos@users.noreply.github.com>
2023-08-27 14:05:53 +0000

    Setting up Tailwind and first styleguide elements

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'yellow': '#ffd371',
+        'blue': '#74c0fa',
+        'pink': '#ffabcd'
+      }
+    },
+  },
+  plugins: [],
+}
+
